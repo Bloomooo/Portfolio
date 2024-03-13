84da511135d0781d25aa8b21954d56aafd45be85
Yanis <mechtayanis20@gmail.com>
2024-03-13 13:03:22 +0100

    update pour les liens des competences

--- app/components/cardgrid.tsx
+++ app/components/cardgrid.tsx
@@ -4,42 +4,43 @@ import { Panel, Row, Col } from "rsuite";
 import "rsuite/dist/rsuite.min.css";
 import { CardProps } from "../interface/cardprops";
 import { CardsGridProps } from "../interface/cardgridprops";
-import Image from "next/image";
 
 const Card: React.FC<CardProps> = ({ competence }) => {
   return competence ? (
-    <Panel
-      bordered
-      className="flex flex-col relative"
-      style={{
-        width: "17rem",
-        height: "10rem",
-        maxWidth: "100%",
-      }}
-    >
-      <div className="flex-grow">
-        <img
-          src={competence.image}
-          className="object-contain"
-          alt={competence.name}
-          style={{
-            maxHeight: "6rem",
-          }}
-        />
-      </div>
-      <div
-        className="absolute top-0 right-0 p-2 text-white"
-        style={{ textAlign: "right" }}
+    <a href={competence.url} target="_blank" rel="noopener noreferrer">
+      <Panel
+        bordered
+        className="flex flex-col relative"
+        style={{
+          width: "17rem",
+          height: "10rem",
+          maxWidth: "100%",
+        }}
       >
-        {competence.maitrise}
-      </div>
-      <div
-        className="absolute bottom-0 right-0 p-2 text-white"
-        style={{ textAlign: "right" }}
-      >
-        {competence.name}
-      </div>
-    </Panel>
+        <div className="flex-grow">
+          <img
+            src={competence.image}
+            className="object-contain"
+            alt={competence.name}
+            style={{
+              maxHeight: "6rem",
+            }}
+          />
+        </div>
+        <div
+          className="absolute top-0 right-0 p-2 text-white"
+          style={{ textAlign: "right" }}
+        >
+          {competence.maitrise}
+        </div>
+        <div
+          className="absolute bottom-0 right-0 p-2 text-white"
+          style={{ textAlign: "right" }}
+        >
+          {competence.name}
+        </div>
+      </Panel>
+    </a>
   ) : null;
 };
 
